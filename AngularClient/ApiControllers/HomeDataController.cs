3983622c1c82f2318d3de3619e203908ee824364
Vladimir Bychkov <bychkov@gmail.com>
2015-04-22 14:05:01 -0400

    Update

---
+++ AngularClient/ApiControllers/HomeDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SPATemplate
+{
+    public class HomeDataController : ApiController
+    {
+        public string Get()
+        {
+            return "Unsecured home data";
+        }
+    }
+}
---
+++ AngularClient/ApiControllers/OtherDataController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SPATemplate
+{
+    [Authorize]
+    public class OtherDataController : ApiController
+    {
+        public string Get()
+        {
+            return "sensitive other data";
+        }
+    }
+}
