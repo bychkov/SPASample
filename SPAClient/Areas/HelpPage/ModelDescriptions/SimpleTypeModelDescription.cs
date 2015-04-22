9bdf026f2d58d70e6de79a987fba39b938654a25
Vladimir Bychkov <bychkov@gmail.com>
2015-04-22 13:43:55 -0400

    Initial commit

---
+++ SPAClient/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SPAClient.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ STS/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Owin;
+using STS;
+using STS.Configuration;
+using Thinktecture.IdentityServer.Core.Configuration;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace STS
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Map("/core", idsrvApp =>
+            {
+                var factory = InMemoryFactory.Create(
+                    users: Users.Get(),
+                    clients: Clients.Get(),
+                    scopes: Scopes.Get());
+
+                var options = new IdentityServerOptions
+                {
+                    SiteName = "Sample STS",
+                    SigningCertificate = Certificate.Load(),
+                    Factory = factory,
+                };
+
+                appBuilder.UseIdentityServer(options);
+            });
+        }
+    }
+}
